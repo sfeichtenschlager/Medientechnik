0fbb70c18e5ad43486e52cbbdb6114f852d0133d
sfeichtenschlager <feichtenschlager.s@gmx.at>
2021-10-19 14:10:34 +0200

    Added folder #6

---
+++ uebungSlideshow02/script.js
@@ -0,0 +1,28 @@
+function clickFirstButton() {
+    document.body.style.backgroundColor = "rgb(176, 255, 238)";
+    document.getElementById('image').src = "images/image01.JPG";
+}
+
+function clickSecButton() {
+    document.body.style.backgroundColor = "rgb(255, 244, 173)";
+    document.getElementById('image').src = "images/image02.JPG";
+}
+
+function clickThirdButton() {
+    document.body.style.backgroundColor = "rgb(255, 128, 128)";
+    document.getElementById('image').src = "images/image03.JPG";
+}
+
+function clickFourthButton() {
+    document.body.style.backgroundColor = "rgb(173, 255, 177)";
+    document.getElementById('image').src = "images/image04.JPG";
+}
+
+function clickFifthButton() {
+    document.body.style.backgroundColor = "rgb(251, 181, 255)";
+    document.getElementById('image').src = "images/image05.JPG";
+}
+
+
+
+
