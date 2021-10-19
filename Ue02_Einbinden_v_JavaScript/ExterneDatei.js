b74921030c747123b81b2a4fef560d9dab024bae
sfeichtenschlager <feichtenschlager.s@gmx.at>
2021-10-19 14:08:49 +0200

    Added folder #4

---
+++ Ue02_Einbinden_v_JavaScript/ExterneDatei.js
@@ -0,0 +1,3 @@
+var zuAenderndeZahl = 444;
+var Fehler = "Fehler leider noch nicht gel&ouml;st - finde ihn";
+var keinFehler = "Fehler gel&ouml;st";
---
+++ Ue02_Einbinden_v_JavaScript/eigene_JavaScript_Datei.js
@@ -0,0 +1,4 @@
+var string = "Ich werde im Jahr ";
+var year = 2023;
+var age = 18;
+var secString = " Jahre alt."
